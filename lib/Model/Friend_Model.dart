06cdeb26f272d90aebeee518fa05dd1378f04065
Shady Emad <Shadygirgis79@gmail.com>
2024-11-16 18:34:57 +0200

    Friend MVC model Done

---
+++ lib/Model/Friend_Model.dart
@@ -0,0 +1,11 @@
+
+
+class Friend {
+  String name;
+  String profileURL;
+  String phoneNumber;
+  int events = 0;
+
+  Friend({required this.name, required this.profileURL,required this.phoneNumber , required this.events});
+
+}
