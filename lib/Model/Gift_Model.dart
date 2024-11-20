d78973ddbca9955603116a55fadcfe732cd845dd
Shady Emad <Shadygirgis79@gmail.com>
2024-11-20 03:40:45 +0200

    Gift Model is Done

---
+++ lib/Model/Gift_Model.dart
@@ -0,0 +1,14 @@
+
+
+class Gift {
+  String name;
+  String category;
+  String status; // Pledged or Unpledged
+  int price;
+
+  Gift({
+    required this.name,
+    required this.category ,
+    required this.status ,
+    required this.price});
+}
