df5cc61d64ee44546f95d1da27c548d4021abd82
Shady Emad <Shadygirgis79@gmail.com>
2024-11-20 03:47:27 +0200

    Main Added new routes navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 ﻿import 'package:flutter/material.dart';
-import 'package:hedieaty/HomePage.dart';
+import 'package:hedieaty/Events/FriendsEventListPage.dart';
+import 'package:hedieaty/Gifts/FriendsGiftListPage.dart';
+import 'package:hedieaty/Home/HomePage.dart';
+import 'package:hedieaty/Events/MyEventsListPage.dart';
+import 'package:hedieaty/Gifts/MyGiftListPage.dart';
+import 'package:hedieaty/MyGiftDetailsPage.dart';
+
 void main() {
   runApp(const HedieatyApp());
 }
@@ -10,14 +16,21 @@ class HedieatyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hedieaty App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(),
+        title: 'Hedieaty App',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        initialRoute: '/Home', // Initial route
+        routes: {
+          '/Home': (context) => const MyHomePage(),
+          '/FriendsEvent': (context) => const FriendsEventList(),
+          '/MyEvents': (context) => const MyEventsList(),
+          '/FriendsGiftList': (context) => const FriendsGiftList(),
+          '/MyGiftList': (context) => const MyGiftList(),
 
+          '/MyGiftDetails': (context) => const MyGiftDetails(),
+        }
     );
   }
 }
-
