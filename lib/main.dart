b6953854fe5d7fff5a156057f08a80f28450560c
Shady Emad <Shadygirgis79@gmail.com>
2024-12-03 17:42:34 +0200

    Adding routes to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 ﻿import 'package:flutter/material.dart';
 import 'package:hedieaty/Registration/SignInPage.dart';
 
+import 'Details/FriendsGiftDetailsPage.dart';
+import 'Details/MyGiftDetailsPage.dart';
+import 'Events/FriendsEventListPage.dart';
+import 'Events/MyEventsListPage.dart';
+import 'Gifts/FriendsGiftListPage.dart';
+import 'Gifts/MyGiftListPage.dart';
+import 'Home/HomePage.dart';
+import 'Profile/ProfilePage.dart';
+
 void main() {
   runApp(const MaterialApp(
     home: HedieatyApp(),
@@ -19,19 +28,17 @@ class HedieatyApp extends StatelessWidget {
           useMaterial3: true,
         ),
         home: SignInPage(),
+        routes: {
+          '/Home': (context) => const MyHomePage(),
+          '/FriendsEvent': (context) => const FriendsEventList(),
+          '/MyEvents': (context) => const MyEventsList(),
+          '/FriendsGiftList': (context) => const FriendsGiftList(),
+          '/MyGiftList': (context) => const MyGiftList(),
+          '/FriendsGiftDetails': (context) => const FriendsGiftDetails(),
+          '/MyGiftDetails': (context) => const MyGiftDetails(),
+          '/Profile': (context) => const ProfilePage(),
 
-      // Initial route
-        // routes: {
-        //   '/Home': (context) => const MyHomePage(),
-        //   '/FriendsEvent': (context) => const FriendsEventList(),
-        //   '/MyEvents': (context) => const MyEventsList(),
-        //   '/FriendsGiftList': (context) => const FriendsGiftList(),
-        //   '/MyGiftList': (context) => const MyGiftList(),
-        //   '/FriendsGiftDetails': (context) => const FriendsGiftDetails(),
-        //   '/MyGiftDetails': (context) => const MyGiftDetails(),
-        //   '/Profile':(context) => const ProfilePage(),
-
-
+        }
     );
   }
 }
