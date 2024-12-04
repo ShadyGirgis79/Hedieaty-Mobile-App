9d4ee33b4087ed129f5bc12b963f9ccd6f2c96ae
Shady Emad <Shadygirgis79@gmail.com>
2024-12-04 09:10:45 +0200

    Authentication file done

---
+++ lib/Database/Authentication.dart
@@ -0,0 +1,41 @@
+
+import 'package:firebase_auth/firebase_auth.dart';
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // Sign In with Email and Password
+  Future<User?> signIn(String email, String password) async {
+    try {
+      UserCredential result = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return result.user;
+    }
+    catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  // Register with Email and Password
+  Future<User?> signUp(String email, String password) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return result.user;
+    }
+    catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  // Sign Out
+  Future<void> signOut() async {
+    await _auth.signOut();
+  }
+}
+
