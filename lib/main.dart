4563be4e3ef2b21e46975fbbcd786bf644101708
Shady Emad <Shadygirgis79@gmail.com>
2024-12-04 09:12:31 +0200

    Check whether user is signed in or not

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
-﻿import 'package:flutter/material.dart';
-import 'package:hedieaty/Registration/SignInPage.dart';
-
+﻿import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'Home/AddFriend.dart';
+import 'Registration/SignInPage.dart';
+import 'Registration/SignUpPage.dart';
 import 'Details/FriendsGiftDetailsPage.dart';
 import 'Details/MyGiftDetailsPage.dart';
 import 'Events/FriendsEventListPage.dart';
@@ -10,14 +13,36 @@ import 'Gifts/MyGiftListPage.dart';
 import 'Home/HomePage.dart';
 import 'Profile/ProfilePage.dart';
 
-void main() {
-  runApp(const MaterialApp(
-    home: HedieatyApp(),
-  ));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(HedieatyApp());
 }
 
-class HedieatyApp extends StatelessWidget {
+class HedieatyApp extends StatefulWidget {
   const HedieatyApp({super.key});
+
+  @override
+  State<HedieatyApp> createState() => _HedieatyAppState();
+
+}
+
+class _HedieatyAppState extends State<HedieatyApp> {
+
+  @override
+  void initState() {
+    FirebaseAuth.instance
+        .authStateChanges()
+        .listen((User? user) {
+      if (user == null) {
+        print('========================================= User is currently signed out!');
+      }
+      else {
+        print('========================================== User is signed in!');
+      }
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,7 +52,9 @@ class HedieatyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: SignInPage(),
+        home: FirebaseAuth.instance.currentUser == null
+            ? SignInPage()
+            : MyHomePage(),
         routes: {
           '/Home': (context) => const MyHomePage(),
           '/FriendsEvent': (context) => const FriendsEventList(),
@@ -37,10 +64,13 @@ class HedieatyApp extends StatelessWidget {
           '/FriendsGiftDetails': (context) => const FriendsGiftDetails(),
           '/MyGiftDetails': (context) => const MyGiftDetails(),
           '/Profile': (context) => const ProfilePage(),
-
+          '/AddFriend': (context) => const AddFriend(),
+          '/SignIn': (context) => const SignInPage(),
+          '/SignUp': (context) => const SignUpPage(),
         }
     );
   }
 }
+
 
 
