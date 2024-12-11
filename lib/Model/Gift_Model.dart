7b1d479585dad296f3e97176bbe2e10c5798dde4
Shady Emad <Shadygirgis79@gmail.com>
2024-12-11 05:05:49 +0200

    Gift Model add attributes

--- lib/Model/Gift_Model.dart
+++ lib/Model/Gift_Model.dart
@@ -4,11 +4,16 @@ class Gift {
   String name;
   String category;
   String status; // Pledged or Unpledged
-  int price;
+  String description;
+  String imageURL;
+  double price;
+
 
   Gift({
     required this.name,
-    required this.category ,
-    required this.status ,
+    required this.category,
+    required this.status,
+    this.description="",
+    this.imageURL="",
     required this.price});
 }
